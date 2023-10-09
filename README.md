60dc7ded4af908a3995f28c7249801efcab24a13
jacdejulio <76726688+jacdejulio@users.noreply.github.com>
2023-10-09 15:31:10 +0800

    Finalized features, UI and README.md

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # etch-a-sketch
 
-This project is my take on the etch-a-sketch.
+This project uses HTML, CSS and JavaScript to mimic the etch-a-sketch toy.
+
+Features:
+* Grid size selector - user can choose from 1-100. When a user chooses a number, the grid size is adjusted by that number multiplied to itself. Default grid size is 16*16.
+* Pen color picker - allows the user to select the pen color.
+* Pen button - allows the user to reselect the pen function after using the erase button.
+* Rainbow button - randomize each square’s RGB value
+* Erase button - allows the user to reset the color per cell
+* Reset button - resets all cell colors
+
+Icons/Images sources:
+
+* <a href="https://www.flaticon.com/free-icons/color-wheel" title="color wheel icons">Color wheel icons created by Freepik - Flaticon</a>
+* <a href="https://www.flaticon.com/free-icons/color-picker" title="color-picker icons">Color-picker icons created by kerismaker - Flaticon</a>
+* <a href="https://www.flaticon.com/free-icons/eraser" title="eraser icons">Eraser icons created by chahir - Flaticon</a>
+* <a href="https://www.flaticon.com/free-icons/menu" title="menu icons">Menu icons created by BomSymbols - Flaticon</a>
+* <a href="https://www.flaticon.com/free-icons/ballpen" title="ballpen icons">Ballpen icons created by Hilmy Abiyyu A. - Flaticon</a>
--- app.js
+++ app.js
@@ -1,121 +1,115 @@
+// DECLARING VARIABLES
+// Grid
+const body = document.querySelector("body");
+const grid = document.querySelector(".container")
+const gridSelector = document.querySelector("#userInput");
+const defGridSize = parseInt(gridSelector.value);
 
-const container = document.querySelector(".container");
-const containerWidth = container.clientWidth;
-const containerHeight = container.clientHeight;
-let square = document.createElement("div");
+// Color
+const colorPicker = document.querySelector("#penColor");
+const btnDraw = document.querySelector(".draw");
+const btnRainbow = document.querySelector(".rainbow");
 const btnReset = document.querySelector(".reset");
 const btnErase = document.querySelector(".erase");
-const btnRainbow = document.querySelector(".rainbow")
-const userInput = document.getElementById("userInput");// Get a reference to the number input element
-let currentValue = userInput.value;
 
-const body = document.querySelector("body")
+body.addEventListener("load", createGrid(defGridSize));
 
 
-body.addEventListener("load", createGrid(16))
+gridSelector.addEventListener("input", function () {
+    let gridValue = parseInt(gridSelector.value);
+    maxValue = parseInt(gridSelector.max)
+    minValue = parseInt(gridSelector.min)
 
-// Add an "input" event listener to the input element
-userInput.addEventListener("input", function () {
-    currentValue = userInput.value;
-    // Retrieve the current value of the input field
-    createGrid(currentValue);
+    if (gridValue > maxValue) {
+        gridValue = maxValue;
+    } else if (gridValue < minValue) {
+        gridValue = minValue;
+    }
+    createGrid(gridValue);
 });
 
 
-//Creates a grid based on a number provided by the user
-function createGrid(currentValue) {
-    // Clear any existing squares
-    container.innerHTML = '';
 
-    for (let column = 0; column < currentValue; column++) {
-        for (let row = 0; row < currentValue; row++) {
+function createGrid(currValue) {
+    grid.innerHTML = ''
+    for (let column = 0; column < currValue; column++) {
+        for (let row = 0; row < currValue; row++) {
             const square = document.createElement("div");
             square.className = "square";
 
-            container.append(square);
+            grid.append(square);
         }
     }
 
-    // Update the grid template columns and rows based on the currentValue
-    container.style.gridTemplateColumns = `repeat(${currentValue}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${currentValue}, 1fr)`;
+    // Update the grid template columns and rows based on the currValue
+    grid.style.gridTemplateColumns = `repeat(${currValue}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${currValue}, 1fr)`;
 
-    colorCells()
+    changeCellsColor();
+
 }
 
 
-//Variable to store the grid cells in an array
 
-function colorCells() {
+function changeCellsColor() {
+    let currState = "draw";
+    let defPenColor = colorPicker.value;
+    let defCellColor = "#d9d9d9";
     const allSquares = document.querySelectorAll(".square");
-    //Function to color cells when hovered on
+
+
     for (let i = 0; i < allSquares.length; i++) {
-        let perSquare = allSquares[i]; // Access each square individually
+        let perSquare = allSquares[i];
+
         perSquare.addEventListener("mouseover", () => {
-            perSquare.style.backgroundColor = "#66666e"; // Set the background color of the clicked square
+            perSquare.style.backgroundColor = defPenColor;
         });
 
     }
 
-    //Reset button function
-    btnReset.addEventListener("click", reset = () => {
-        for (let i = 0; i < allSquares.length; i++) {
-            let perSquare = allSquares[i]; // Access each square individually
-            perSquare.style.backgroundColor = "#d9d9d9"; // Set the background color of the clicked square
-        }
+
+    colorPicker.addEventListener("input", () => {
+        let currColor = colorPicker.value
+        changeCellsColor(currColor);
     })
+
+    btnDraw.addEventListener("click", () => {
+        let currColor = colorPicker.value
+        changeCellsColor(currColor);
+    })
+
 
     btnRainbow.addEventListener("click", () => {
         for (let i = 0; i < allSquares.length; i++) {
-            let perSquare = allSquares[i]; // Access each square individually
+            let perSquare = allSquares[i];
             perSquare.addEventListener("mouseover", () => {
                 let r = Math.floor(Math.random() * 256);
                 let g = Math.floor(Math.random() * 256);
                 let b = Math.floor(Math.random() * 256);
-                perSquare.style.backgroundColor = `rgb(${r},${g},${b})`; // Set the background color of the clicked square
+                perSquare.style.backgroundColor = `rgb(${r},${g},${b})`;
             });
 
         }
     })
-
 
     btnErase.addEventListener("click", () => {
         for (let i = 0; i < allSquares.length; i++) {
-            let perSquare = allSquares[i]; // Access each square individually
+            let perSquare = allSquares[i];
             perSquare.addEventListener("mouseover", () => {
-                perSquare.style.backgroundColor = "#d9d9d9"; // Set the background color of the clicked square
-            });
-
-        };
-
-    })
-
-
-    const picker = document.getElementById("penColor")
-
-    picker.addEventListener("input", () => {
-        let currColor = picker.value;
-        console.log(currColor)
-
-        for (let i = 0; i < allSquares.length; i++) {
-            let perSquare = allSquares[i]; // Access each square individually
-            perSquare.addEventListener("mouseover", () => {
-                perSquare.style.backgroundColor = currColor; // Set the background color of the clicked square
+                perSquare.style.backgroundColor = defCellColor;
             });
 
         }
     })
 
+    btnReset.addEventListener("click", reset = () => {
+        allSquares.forEach(square => {
+            square.style.backgroundColor = defCellColor;
+        });
+    });
+
+
 }
-
-
-
-
-
-
-
-
-
 
 
 
